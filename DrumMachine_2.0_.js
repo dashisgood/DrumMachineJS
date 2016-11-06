38bacbed815861195065307cae64e5b835d1bf84
Max Garvey <maxgarvey@gmail.com>
2016-11-05 18:09:36 -0700

    add semicolons.

--- DrumMachine_2.0_.js
+++ DrumMachine_2.0_.js
@@ -13,7 +13,7 @@ function loadSound(url, sound) {
       allBuffers[sound] = buffer;
       console.log(allBuffers);
     });
-  }
+  };
   request.send();
 }
 
@@ -21,7 +21,7 @@ var allBuffers = {
 	'bd':null,
 	'sn':null,
 	'hh':null,
-}
+};
 
 loadSound('sounds/bd.wav', 'bd');
 loadSound('sounds/sn.wav', 'sn');
@@ -59,31 +59,27 @@ function pauseLoop(){
 function playBeat(){
 
 	drawNewBar();
-	if(beat===16||beat===0){stepCounters[15].style.backgroundColor = 'grey'}
-	else{stepCounters[beat-(bar*16)-1].style.backgroundColor = 'grey'}
-	stepCounters[beat-(bar*16)].style.backgroundColor = '#ffff00'
-	for(drum of [hh, sn, bd]){ 
+	if(beat===16||beat===0){stepCounters[15].style.backgroundColor = 'grey';}
+	else{stepCounters[beat-(bar*16)-1].style.backgroundColor = 'grey';}
+	stepCounters[beat-(bar*16)].style.backgroundColor = '#ffff00';
+	for(let drum of [hh, sn, bd]){ 
 		if (drum.steps[beat]===1){
-			drum.sound()
+			drum.sound();
 		} 
 	}
 	if (beat===15){
 		beat++;
 		bar++;
 		
-
-		return
+		return;
 	}
 	if (beat===31){
 		beat = 0;
 		bar = 0;
 		
-		return
-
+		return;
 	}
-	beat++;;
-
-	
+	beat++;
 
 }
 
@@ -92,13 +88,13 @@ function toggleStep(drum, step) {
 	if (drums[drum].steps[step + (bar*16)] === 0){
 		drums[drum].steps[step + (bar*16)] = 1;
 		drums[drum].stepElements[step].style.backgroundColor = 'c12e2a';
-		return
+		return;
 	}
 	if (drums[drum].steps[step + (bar*16)] === 1){
 		drums[drum].steps[step + (bar*16)] = 0;
 		drums[drum].stepElements[step].style.backgroundColor = 'black';
 	}
-};
+}
 
 function drawNewBar(){
 
@@ -110,7 +106,7 @@ function drawNewBar(){
 			else{
 				drums[d].stepElements[i].style.backgroundColor = 'black';
 			}
-			stepCounters[i].style.backgroundColor = 'grey'
+			stepCounters[i].style.backgroundColor = 'grey';
 		}
 	}
 	barCounter.innerHTML = "Bar " + (bar + 1) + "." + (beat-bar*16+1);
@@ -136,19 +132,19 @@ var hh = {
 		"steps":[0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,], 
 		"sound": function(){playSound(allBuffers.hh);},
 		"stepElements": document.getElementsByClassName('step-button-hh')
-	}
+	};
 var sn = {
 		"steps":[0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,], 
 		"sound": function(){playSound(allBuffers.sn);},
 		"stepElements": document.getElementsByClassName('step-button-sn')
-	}
+	};
 var bd = {
 		"steps":[0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,], 
 		"sound": function(){playSound(allBuffers.bd);},
 		"stepElements": document.getElementsByClassName('step-button-bd')
-	}
+	};
 
-var drums = [hh, sn, bd]
+var drums = [hh, sn, bd];
 var bar = 0;
 var beat = 0;
 var totalBars = 2;
@@ -156,4 +152,3 @@ var totalBars = 2;
 var stepCounters = document.getElementsByClassName('step-counter');
 var barCounter = document.getElementById('bar-counter');
 drawNewBar();
-
